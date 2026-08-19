139c93f2f1e1601ee537116181980abb0ddf5cec
Vector <dsafdsaddsa@gmail.com>
2026-08-19 20:29:26 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,15 @@
+getfenv(2).test = function()
+  return print("hi2")
+end
+
+getfenv(3).test = function()
+  return print("hi1")
+end
+
+getfenv(1).test = function()
+  return print("hi3")
+end
+
 getfenv().test = function()
-  return print("hi")
+  return print("hi4")
 end
